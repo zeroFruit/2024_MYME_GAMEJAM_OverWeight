41fab3e5ac855617ec07e9ac5c012e4f43680345
borre <myme@juhyung.dev>
2024-07-14 01:48:54 +0900

    ui progress bar 코드 추가

--- Assets/Scripts/UI/HUD/HUDDayProgress.cs
+++ Assets/Scripts/UI/HUD/HUDDayProgress.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class HUDDayProgress : MonoBehaviour
 {
+    public float progress = 0;
+
+    public bool debug = false;
+    public UIProgressBar progressBar;
+
+    void Awake() {
+        if (progressBar == null) {
+            Debug.LogError("HUDDayProgress: progressBar is not set");
+        }
+    }
+
     /**
      * progress: 0~1
      */
     public void UpdateDayProgress(float progress)
     {
+        if (progress < 0 || progress > 1) {
+            Debug.LogError("HUDDayProgress: progress should be 0~1");
+            return;
+        }
+        this.progressBar.UpdateBar01(progress);
+    }
+
+    void Update() {
+        if (debug) {
+            if (progress >= 1) {
+                progress = 1;
+            }
+            if (progress <= 0) {
+                progress = 0;
+            }
+            UpdateDayProgress(progress);
+        }
     }
 }
