7a7d55a2fa89edb26216b8a65635e9d1712a6357
borre <myme@juhyung.dev>
2024-07-14 03:06:28 +0900

    elevator setting 자식 세팅 레퍼런스 추가

--- Assets/Scripts/UI/UIPanelElevatorSetting.cs
+++ Assets/Scripts/UI/UIPanelElevatorSetting.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +6,17 @@ public class UIPanelElevatorSetting : UIPanel
 {
     [Header("Binding")]
     public Button CloseButton;
+    public List<UISectionElevatorStatus> elevatorSettingUIs = new List<UISectionElevatorStatus>();
 
     protected override void Awake()
     {
         base.Awake();
         this.CloseButton.onClick.AddListener(this.OnClickClose);
+
+        if (elevatorSettingUIs.Count != 3)
+        {
+            Debug.LogError($"UIPanelElevatorSetting: elevatorSettingUIs is not three {elevatorSettingUIs.Count}");
+        }
     }
 
     void OnClickClose()
