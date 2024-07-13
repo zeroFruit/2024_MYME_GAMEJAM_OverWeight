a93375c9f4568d335a9128b2e6583128868d4a16
borre <myme@juhyung.dev>
2024-07-14 01:38:22 +0900

    HUD day progress script 추가

---
+++ Assets/Scripts/UI/HUD/HUDDayProgress.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDDayProgress : MonoBehaviour
+{
+    /**
+     * progress: 0~1
+     */
+    public void UpdateDayProgress(float progress)
+    {
+    }
+}
