548212c19ceb9a3baa94896d0b8f45f9cd1c0622
borre <myme@juhyung.dev>
2024-07-14 04:43:05 +0900

    elevator 최대 탑승자수 기록

--- Assets/Scripts/UI/UIElevatorCapacity.cs
+++ Assets/Scripts/UI/UIElevatorCapacity.cs
@@ -1,16 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class UIElevatorCapacity : MonoBehaviour
 {
     public int elevatorIndex;
-
+    public TMP_Text elevatorCapacityText;
     public void SetElevatorIndex(int index)
     {
         elevatorIndex = index;
     }
 
     void Update() {
-
-
+        var elevatorManager = ElevatorManager.Instance;
+        if (elevatorManager._elevators.Count <= elevatorIndex) {
+            return;
+        }
+        var elevator = ElevatorManager.Instance._elevators[elevatorIndex];
+        elevatorCapacityText.text = elevator._maxCapacity + " 명";
     }
 }
